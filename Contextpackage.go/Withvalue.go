d8558f454105376a1d8c68257b150910b14a1790
abishekkumar2001 <abishekkumar.k@datasirpi.com>
2024-01-18 15:58:27 +0530

    Context Package in Go

--- Contextpackage.go/Withvalue.go
+++ Contextpackage.go/Withvalue.go
@@ -3,19 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 )
 
-type key int
-
-const keyvalue key = 12345
+func myfunc(ctx context.Context) {
+	fmt.Printf("myfunc: key's value is %s\n", ctx.Value("key"))
+}
 
 func main() {
-	ctx := context.WithValue(context.Background(), keyvalue, 23456)
-	keyvalue1 := ctx.Value(keyvalue)
-	i, ok := keyvalue1.(int)
-	if !ok {
-		log.Fatal("not an integer")
-	}
-	fmt.Println("value: ", i)
+	ctx := context.Background()
+
+	ctx = context.WithValue(ctx, "key", "Animal")
+
+	myfunc(ctx)
+
 }
